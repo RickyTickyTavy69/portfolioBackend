538dbef60caa7397f95a843834a79658cbbd7931
Artem Bakirov <a.bakirov@eclever.io>
2023-10-01 22:55:18 +0200

    feat: save user data to db

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: "http://localhost:3000/Portfolio_Astro/"
+  });
   await app.listen(5000);
 }
 bootstrap();
