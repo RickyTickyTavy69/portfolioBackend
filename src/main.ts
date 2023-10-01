72e2d920f478466bd7ae3622d45290bf7aa0d0f3
Artem Bakirov <a.bakirov@eclever.io>
2023-10-02 00:16:38 +0200

    feat: remove origin from cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: "https://rickytickytavy69.github.io"
-  });
+  const app = await NestFactory.create(AppModule, {cors: true});
+  app.enableCors();
   await app.listen(5000);
 }
 bootstrap();
