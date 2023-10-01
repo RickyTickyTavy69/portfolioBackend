e04719fadb97a1431bd7854e8fb4ce9c01e982cd
Artem Bakirov <a.bakirov@eclever.io>
2023-10-01 23:39:52 +0200

    feat: changed cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:3000/Portfolio_Astro/"
+    origin: "https://rickytickytavy69.github.io"
   });
   await app.listen(5000);
 }
